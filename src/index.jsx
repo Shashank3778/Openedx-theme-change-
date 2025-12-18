ad84dfc2cf279c0f9e33d18b31dad3423ff1c665
Shashank3778 <anku3778@gmail.com>
2025-12-18 11:19:51 +0530

    make the footer correct

--- src/index.jsx
+++ src/index.jsx
@@ -12,8 +12,7 @@ import { createRoot } from 'react-dom/client';
 import { Route, Routes, Outlet } from 'react-router-dom';
 
 import Header from '@edx/frontend-component-header';
-import { FooterSlot } from '@edx/frontend-component-footer';
-
+import Footer from '@edx/frontend-component-footer';
 import configureStore from './data/configureStore';
 import AccountSettingsPage, { NotFoundPage } from './account-settings';
 import IdVerificationPageSlot from './plugin-slots/IdVerificationPageSlot';
@@ -35,7 +34,7 @@ subscribe(APP_READY, () => {
               <main className="flex-grow-1" id="main">
                 <Outlet />
               </main>
-              <FooterSlot />
+              <Footer />
             </div>
         )}
           >
